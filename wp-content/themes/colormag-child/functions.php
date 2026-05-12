ce3f9d5ab0df6bd7073a227d79b96761cf5a5b53
Lorenzo Lepori <lorenzo@MacBook-Pro-di-Lorenzo.local>
2026-05-12 16:04:23 +0200

    Tipografia: aggiunto font Merriweather per i titoli istituzionali

--- wp-content/themes/colormag-child/functions.php
+++ wp-content/themes/colormag-child/functions.php
@@ -3,4 +3,10 @@
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style' );
 function enqueue_parent_theme_style() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+    wp_enqueue_style(
+        'google-fonts-merriweather',
+        'https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&display=swap',
+        array(),
+        null
+    );
 }
